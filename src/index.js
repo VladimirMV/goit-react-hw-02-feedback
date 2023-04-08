893b47f5278b43d9606a3bb3c504ddfe528f2c02
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-04-09 00:35:48 +0300

    ДЗ 2 часть 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+// reportWebVitals();
